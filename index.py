5f5faf48e1059f1c84647c4442e14c7da3f598fc
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2023-04-09 14:56:07 +0200

    success stage 1

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-import os
 import time
+import pytesseract
+import cv2
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 directory = 'media'
 
@@ -10,7 +13,14 @@ class LatestImageHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory and event.src_path.endswith('.jpg') or event.src_path.endswith('.jpeg') or event.src_path.endswith('.png'):
             print(f"The latest uploaded image is now: {event.src_path}")
-
+            print(f"{event.src_path}")
+            img = cv2.imread(f"{event.src_path}")
+            # print(img)
+            config = ('-l eng --oem 1 --psm 3')
+            text = pytesseract.image_to_string(img, config=config)
+            # print text
+            text = text.split('\n')
+            print(text)
 
 # Create a handler for file system events
 event_handler = LatestImageHandler()

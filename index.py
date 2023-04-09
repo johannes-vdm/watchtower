53d443862c9f0eac5f7117d7a57078cc48bb9e05
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2023-04-09 18:03:07 +0200

    phase 2 complete

--- index.py
+++ index.py
@@ -4,10 +4,12 @@ import cv2
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+from openai_functions import openai_response
 
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 directory = 'media'
 
+initialResp = 'What is the correct answer out of the following:'
 
 class LatestImageHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -15,15 +17,22 @@ class LatestImageHandler(FileSystemEventHandler):
             print(f"The latest uploaded image is now: {event.src_path}")
             print(f"{event.src_path}")
             img = cv2.imread(f"{event.src_path}")
+            response = None
             try:
                 config = ('-l eng --oem 1 --psm 3')
                 text = pytesseract.image_to_string(img, config=config)
-                # print text
-                text = text.split('\n')
                 print(text)
+                text = text.split('\n')
+                response = text
             except TypeError:
                 print(f"Text could not be extracted: {event.src_path}")
-                return
+
+            if (response):
+                print(openai_response(prompt=(initialResp+response)))
+            else:
+                prompt = 'what is the universe about?'
+                openai_response(prompt=prompt)
+                print(openai_response(prompt=prompt))
 
 
 # Create a handler for file system events
---
+++ openai_functions.py
@@ -0,0 +1,25 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_engine = "text-davinci-003"
+
+
+def openai_response(prompt,
+                    model=model_engine,
+                    temperature=0,
+                    max_tokens=60,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0):
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty
+    ).choices[0].text
+
+    return response
